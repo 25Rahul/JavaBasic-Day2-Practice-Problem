26471a971e80fb69aa1e406df71d35523b1379f0
Rahul Gupta <111953769+25Rahul@users.noreply.github.com>
2024-02-12 23:28:33 +0530

    Add files via upload

--- src/com/bridgelabz/SwitchStatements/CheckVowelsConsonant.java
+++ src/com/bridgelabz/SwitchStatements/CheckVowelsConsonant.java
@@ -1,2 +1,19 @@
-package com.bridgelabz.SwitchStatements;public class CheckVowelsConsonant {
+package com.bridgelabz.SwitchStatements;
+
+public class CheckVowelsConsonant {
+    public static void main(String[] args) {
+        char ch = 'e';
+        switch (ch) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                System.out.println(ch+ " is a Vowel");
+                break;
+            default:
+                System.out.println(ch + " is a Consonant");
+
+        }
+    }
 }
